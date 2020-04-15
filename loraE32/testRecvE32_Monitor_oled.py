b9fb50514c2fa2c505500039da2e39b76d9fed1c
Effevee <effevee@gmail.com>
2020-04-15 15:38:05 +0200

    added checksum verification to loraE32

--- loraE32/testRecvE32_Monitor_oled.py
+++ loraE32/testRecvE32_Monitor_oled.py
@@ -6,12 +6,19 @@
 # receiver    - address FFFF - channel 04
 ###########################################
 
+from machine import Pin, I2C
 from loraE32 import ebyteE32
+import ssd1306
 import utime
 
 M0pin = 25
 M1pin = 26
 AUXpin = 27
+oled_i2c_address = 0x3C # decimaal 60
+
+# i2c, oled en e32 instances
+i2c = I2C(scl=Pin(21), sda=Pin(22))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr = oled_i2c_address)
 
 e32 = ebyteE32(M0pin, M1pin, AUXpin, Address=0xFFFF, Channel=0x04, debug=False)
 
@@ -21,9 +28,11 @@ from_address = 0x0001
 from_channel = 0x02
 
 while True:
-    print('Receiving fixed monitor : address %d - channel %d'%(from_address, from_channel), end='')
     message = e32.recvMessage(from_address, from_channel, useChecksum=True)
-    print(' - message %s'%(message))
-    utime.sleep_ms(5000)
+    oled.fill(0)
+    oled.text(message.get('msg'), 0, 0)
+    oled.show()
+    utime.sleep_ms(2000)
 
 e32.stop()
+oled.poweroff()
